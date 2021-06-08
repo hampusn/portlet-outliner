372e72474db1dfd075dcc8fa2eb401b84196abb3
Hampus Nordin <nordin.hampus@gmail.com>
2021-06-08 09:12:36 +0200

    Bumped minor in files.

--- portlet-outliner.js
+++ portlet-outliner.js
@@ -1,7 +1,7 @@
 /**
  * Portlet Outliner
  * @author Hampus Nordin <nordin.hampus@gmail.com>
- * @version 0.1.0
+ * @version 0.2.0
  */
 
 /* Use https://jscompress.com/ (or other online minifier) to compress the script below. */
